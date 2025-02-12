fcec3493122645baff1eaec586655bfc618e10a7
Ascnbeta <146746707+Ascnbeta@users.noreply.github.com>
2025-02-12 11:59:17 +0800

    寒假集训

---
+++ codes/other oj/S2OJ187.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 2.5e5+10;
+int n,l,r;
+int c[maxn];
+int main () {
+	cin >> n >> l;
+	for (int i = 1; i <= n; i++) cin >> c[i];
+	int 
+	return 0;
+}
