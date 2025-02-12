c5e1df371c9570b3c1aedf1d21da9b26e37ab2ef
Ascnbeta <146746707+Ascnbeta@users.noreply.github.com>
2025-02-12 11:56:36 +0800

    寒假集训

---
+++ codes/atcoder/AT_abc387_f.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 2e3+50;
+int n,m;
+int a[maxn];
+int main () {
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) cin >> a[i];
+	
+	return 0;
+}
