04188969930c35a4a036b7bc60228c7756472756
Ascnbeta <2327265401@qq.com>
2025-03-08 15:06:37 +0800

    merge

---
+++ codes/luogu/P10471.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 1e5+30;
+int n;
+int a[maxn];
+int main () {
+#ifdef LOCAL
+	freopen("D:\\codes\\exe\\a.in","r",stdin);
+	freopen("D:\\codes\\exe\\a.out","w",stdout);
+#endif
+	cin >> n;
+	for (int i = 1; i <= n; i++) cin >> a[i];
+		
+	return 0;
+}
