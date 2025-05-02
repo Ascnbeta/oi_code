3b05bccc57a95125b94bd4a47d90a867ae4fa898
Ascnbeta <146746707+Ascnbeta@users.noreply.github.com>
2025-05-02 08:31:30 +0800

    5/2

--- luogu/P2151.cpp
+++ luogu/P2151.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+const int maxn = 54;
+const int maxm = 64;
+int t,a,b;
+struct edge{
+    int u,v;
+}
 int main () {
 #ifdef LOCAL
     freopen("D:/codes/exe/a.in","r",stdin);
