0ca100010182262800d91ba5da5ab6286d9b1970
Ascnbeta <146746707+Ascnbeta@users.noreply.github.com>
2025-05-02 08:10:02 +0800

    5/2

---
+++ luogu/P2151.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+#ifdef LOCAL
+    freopen("D:/codes/exe/a.in","r",stdin);
+    freopen("D:/codes/exe/a.out","w",stdout);
+#endif
+    
+    return 0;
+}
