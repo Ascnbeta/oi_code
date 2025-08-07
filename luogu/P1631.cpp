bdc45e251a112ee782698351bc4c8e0bb21a043a
Ascnbeta <146746707+Ascnbeta@users.noreply.github.com>
2025-08-07 08:57:48 +0800

    更新模板

--- luogu/P1631.cpp
+++ luogu/P1631.cpp
@@ -18,13 +18,35 @@ template <typename T>
 inline void write(T x) {
     if (x<0) x=-x,putchar('-');
     if (x/10) write(x/10);
-    putchar(x%10+'a');
+    putchar(x%10+'0');
 }
+const int maxn = 1e5+5;
+int n,a[maxn],b[maxn];
+struct node{
+    int i,j;
+    bool operator < (const node &y) const{
+        return a[i]+b[j] > a[y.i]+b[y.j];
+    }
+};
+priority_queue<node> q;
 int main () {
 #ifdef LOCAL
     freopen("D:/codes/exe/a.in","r",stdin);
     freopen("D:/codes/exe/a.out","w",stdout);
 #endif
-
+    read(n);
+    for (int i = 1; i <= n; i++) read(a[i]);
+    for (int i = 1; i <= n; i++) read(b[i]);
+    sort(a+1,a+n+1);
+    sort(b+1,b+n+1);
+    q.push({1,1});
+    int n1 = n;
+    while (n1--) {
+        write(a[q.top().i]+b[q.top().j]),putchar(' ');
+        int p = q.top().i,Q = q.top().j;
+        q.pop();
+        if (p+1 <= n) q.push({p+1,Q});
+        if (Q+1 <= n) q.push({p,Q+1});
+    }
     return 0;
 }
